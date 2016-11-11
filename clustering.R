c034d3b8643319a3701eab864c8927f61cb40d41
Joe Heywood <hjodqssh@ioe.ac.uk>
2016-11-11 17:30:27 +0000

    More adjustments to cluster. looking at lda

--- clustering.R
+++ clustering.R
@@ -1,3 +1,4 @@
+source("text_processing.R")
 fls <- dir("../csv/restos/", full.names = T)
 aa <- prepRestDF(fls[1])
 bb <- aa %>% unnest_tokens(sentence, content, token = "sentences") %>% 
@@ -23,4 +24,21 @@ getClusterInfo <- function(x, wc) {
     wcsid <- wc[which(wc$sid == 1),]
     a <- as.data.frame(table(wcsid$cluster), stringsAsFactors = F) %>% arrange(-Freq)
     a$Var1[1]
+}
+
+getHighestScore <- function(x, mx) {
+    r <- mx[x,]
+    mxScore <- max(r)
+    data.frame(ldaTopic = which(r == max(r))[1], beta = mxScore, stringsAsFactors = F)
+}
+
+getSentenceLDA <- function(sid, df, k = 4) {
+    sidDf <- df[which(df$sid == sid), ]
+    kScores <- rep(0, k)
+    for(r in 1:nrow(sidDf)) {
+        rowTopic <- sidDf$ldaTopic[r]
+        rowBeta <- sidDf$beta[r]
+        kScores[rowTopic] <- kScores[rowTopic] + rowBeta
+    }
+    which(kScores == max(kScores))[1]
 }
